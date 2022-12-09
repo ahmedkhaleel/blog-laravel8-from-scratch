6cd5bda4cf674f41a30ec3cb12d61631155347ac
ahmedkhaleel <duZhuqkyptu8barnuz>
2022-12-09 22:59:09 +0200

    Blade: The Absolute Basics

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,8 @@
 <link href="/app.css" rel="stylesheet">
 <article>
-   <h1> <?= $post->title; ?></h1>
+   <h1>{{$post->title}}</h1>
     <div>
-        <?= $post->body; ?>
+        {!! $post->body !!}
     </div>
 
 </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,13 +4,16 @@
 
 
 <body>
-    <?php foreach ($posts as $post): ?>
+    @foreach ($posts as $post)
+        @dd($loop)
     <article>
         <h1>
-            <a href="posts/<?= $post->slug ?>"><?= $post->title ?></a>
+            <a href="posts/{{$post->slug}}">
+                {{$post->title}}
+            </a>
         </h1>
-        <div><?= $post->excerpt ?></div>
+        <div>{{$post->excerpt}}</div>
     </article>
-    <?php endforeach; ?>
+    @endforeach
 
     </body>
